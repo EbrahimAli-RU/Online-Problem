2d7c65370f3db3f00203402de70defba322d5d82
EbrahimAli-RU <37172850+EbrahimAli-RU@users.noreply.github.com>
2019-10-13 17:16:44 +0600

    Mahmoud and a Triangle
    learn non-degenerate triangle

---
+++ 766_B.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    sort(arr,arr+n);
+    for(int i=1;i<n-1;i++)
+    {
+        if(arr[i-1]+arr[i]>arr[i+1])
+        {
+            cout<<"YES"<<endl;
+            return 0;
+        }
+    }
+        cout<<"NO"<<endl;
+
+}
